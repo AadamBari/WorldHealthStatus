fd057b8165f85edcae2e857d67d275dcdf39443c
Aadam Bari <aadambari@msn.com>
2018-09-02 21:40:38 +0100

    created view for meningitis prevalence

--- map/views.py
+++ map/views.py
@@ -47,6 +47,22 @@ def menin_mortality(request):
 
     return render(request, 'menin_mortality.html', context)
 
+
+def menin_prevalence(request):
+    all_diseases = Disease.objects.filter(gho__iexact="MENING_2", year__exact=2014).values('country', 'numeric')
+
+    altered_diseases = list(all_diseases)
+    # print(altered_diseases)
+
+    context = {
+        'diseases': altered_diseases,
+        'disease_name': get_disease_parameter("MENING_2"),
+        'disease_year': 2014,
+    }
+
+    return render(request, 'menin_prevalence.html', context)
+
+
 def cholera_mortality(request):
     all_diseases = Disease.objects.filter(gho__iexact="CHOLERA_0000000002", year__exact=2016).values('country', 'numeric')
 
@@ -79,7 +95,8 @@ def cholera_prevalence(request):
 # function to return disease parameter (ie name of disease statistic)
 def get_disease_parameter(key):
     diseases_dict = {'HIV_0000000006': 'HIV Mortality Rate', 'TB_e_mort_exc_tbhiv_num': 'TB Mortality Rate',
-                     'MENING_1': 'Meningitis mortality rate', 'CHOLERA_0000000002': 'Cholera Mortality Rate', 'CHOLERA_0000000001': 'Cholera Prevalence (Cases)'}
+                     'MENING_1': 'Meningitis mortality rate', 'CHOLERA_0000000002': 'Cholera Mortality Rate',
+                     'CHOLERA_0000000001': 'Cholera Prevalence (Cases)', 'MENING_2': 'Meningitis Prevalence (Cases)'}
     name = diseases_dict[key]
     return name
 
